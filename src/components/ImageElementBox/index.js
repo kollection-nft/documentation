7403e011f56ae6e5099069b11120c0a4bee43317
vonlooten <kqkqkqkqkqkqkq@pm.me>
2023-04-06 23:31:16 +0200

    chore: init

---
+++ src/components/ImageElementBox/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+import Link from '@docusaurus/Link';
+const FeatureList = [
+  {
+    title: 'Developer Docs',
+    src: 'img/dev.png',
+    description: (
+      <>
+        You are a developer and want to launch your own NFT collection on Koinos? Perfect. We have the docs! Read and learn.
+      </>
+    ),
+    link: "/docs/developers",
+    linktext: "LEARN MORE"
+  },
+  {
+    title: 'User Docs',
+    src: 'img/customer.png',
+    description: (
+      <>
+        Learn the basics of NFTs and Kollection, the digital collectibles marketplace at Koinos blockchain.
+      </>
+    ),
+    link: "/docs/users",
+    linktext: "LEARN MORE"
+  },
+];
+
+function Feature({ src, title, description, link, linktext }) {
+  return (
+    <div className={clsx('col col--6')}>
+      <div className="text--center">
+        <img className={styles.img} role="img" src={src} />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <Link
+          className="button button--secondary button--lg"
+          to={link}>
+          {linktext}
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row flex-between">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
